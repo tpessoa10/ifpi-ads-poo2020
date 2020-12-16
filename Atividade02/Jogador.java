0e9715e184ce462995e567c5f5862a04abf59eaa
tpessoa10 <48775460+tpessoa10@users.noreply.github.com>
2020-12-15 23:42:41 -0300

    Add files via upload

---
+++ Atividade02/Jogador.java
@@ -0,0 +1,12 @@
+public class Jogador {
+    int forca;
+    int nivel;
+    int pontos;
+
+
+    void jogador(int force, int level, int points){
+        forca = force;
+        nivel = level;
+        pontos = points;
+    }
+}
